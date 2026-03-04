7ed96d0d8758237ad23e9bc7a566ecef1eb4887e
briandaniel14 <briandaniel789@gmail.com>
2026-03-04 14:02:01 +0000

    Add script

--- classification/train_files/eva_x/chexpert/ft_laterals_vit_ti.sh
+++ classification/train_files/eva_x/chexpert/ft_laterals_vit_ti.sh
@@ -1,11 +1,11 @@
-DATASET_DIR="$HOME/EVA-X/data/CheXpert-v1.0-small/"
+DATASET_DIR="$HOME/repos/EVA-X/data/CheXpert-v1.0-small/"
 CKPT_DIR='checkpoints/eva_x_tiny_patch16_merged520k_mim.pt'
-SAVE_DIR='./output/chexpert/vit_ti_eva_x_chexpert_lateral_lateral5'
+SAVE_DIR='./output/chexpert/vit_sm_eva_x_chexpert_lateral_chexpert5'
 
 # Build a patient-level lateral-only split so validation isn't tiny.
-TRAIN_LIST="$DATASET_DIR/laterals/train.csv"
+TRAIN_LIST="$DATASET_DIR/train.csv"
 VAL_LIST='./'  # not used by train.py
-TEST_LIST="$DATASET_DIR/laterals/valid.csv"  # used as validation set during training
+TEST_LIST="$DATASET_DIR/valid.csv"  # used as validation set during training
 
 
 NUM_GPUS=1 # was 4
@@ -19,7 +19,7 @@ OMP_NUM_THREADS=1 python -m torch.distributed.launch \
     --use_env train.py \
     --dataset chexpert \
     --chexpert_view lateral \
-    --chexpert_labels lateral5 \
+    --chexpert_labels chexpert5 \
     --input_size 224 \
     --finetune ${CKPT_DIR} \
     --output_dir ${SAVE_DIR} \
